f400a9cde004a334784658df2adf7017b7b13c77
s8353480-bit <s8353480@gmail.com>
2026-06-25 20:58:27 +0530

    added new file html css and c++ files

---
+++ .cpp/1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;    
+int main()    
+{    
+   //data type -int, float, double, char, bool,short,byte;
+   //size-of
+   //variable containers
+   //syntax data type variable_name = value;
+   //constant keyboard
+   //const int a = 10;//assigned
+   int b;//declare
+   b=20;//assigned
+   //b=30;
+   //cout<<a<<endl;
+   cout<<b<<endl;
+   int x=10;
+   int y=20;
+   int temp;
+   //x=20;b=10;
+   temp=x;
+    x=y;
+    y=temp;
+    cout<<"value of x is "<<x<<endl;
+    cout<<"value of y is "<<y<<endl;
+    cout<<"helloworld";
+    return 0;
+}   
